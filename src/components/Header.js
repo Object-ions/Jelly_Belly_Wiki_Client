53657f99cc19f7cef74c4b7171cfa9fe868af640
object-ions <moshikoatia@gmail.com>
2023-12-08 14:58:29 -0800

    Add basic content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import LogoFav from '../img/fivcon-final.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <div className="main-nav">
+      <img src={LogoFav} alt="Jwlly Belly Wiki" />
+      <ul>
+        <li className="current"><a href='#'>Home</a></li>
+        <li className=""><a href='#'>Beans</a></li>
+        <li className=""><a href='#'>Facts</a></li>
+        <li className=""><a href='#'>Recipes</a></li>
+        <li className=""><a href='#'>About</a></li>
+        <li className=""><a href='#'>Official Website</a></li>
+      </ul>
+      <ul>
+        <li className=""><a href='#'>API</a></li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header
