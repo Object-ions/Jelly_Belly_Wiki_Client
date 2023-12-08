f213525eeccc6fab87dd35f5e7faf031552537d9
object-ions <moshikoatia@gmail.com>
2023-12-08 15:26:58 -0800

    Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import LogoFav from '../img/fivcon-final.png';
+import React from "react";
+import LogoFav from "../img/fivcon-final.png";
 
 const Header = () => {
   return (
     <div className="main-nav">
-      <img src={LogoFav} alt="Jwlly Belly Wiki" />
-      <ul>
-        <li className="current"><a href='#'>Home</a></li>
-        <li className=""><a href='#'>Beans</a></li>
-        <li className=""><a href='#'>Facts</a></li>
-        <li className=""><a href='#'>Recipes</a></li>
-        <li className=""><a href='#'>About</a></li>
-        <li className=""><a href='#'>Official Website</a></li>
-      </ul>
-      <ul>
-        <li className=""><a href='#'>API</a></li>
-      </ul>
+      <div className="container">
+        <img src={LogoFav} alt="Jwlly Belly Wiki" />
+        <ul>
+          <li className="current">
+            <a href="#">Home</a>
+          </li>
+          <li className="">
+            <a href="#">Beans</a>
+          </li>
+          <li className="">
+            <a href="#">Facts</a>
+          </li>
+          <li className="">
+            <a href="#">Recipes</a>
+          </li>
+          <li className="">
+            <a href="#">About</a>
+          </li>
+          <li className="">
+            <a href="#">Official Website</a>
+          </li>
+        </ul>
+        <ul>
+          <li className="">
+            <a href="#">API</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
