87b20055b7d681626092bd4505315c9754c7931a
object-ions <moshikoatia@gmail.com>
2023-12-08 20:00:29 -0800

    Add component

--- src/components/pages/DisplayControl.js
+++ src/components/pages/DisplayControl.js
@@ -1,11 +1,13 @@
 import React from "react";
 import InformativeSection from "../InformativeSection";
 import LandingSection from "../LandingSection";
+import DisplayHistory from "../DisplayHistory";
 
 const DisplayControl = () => {
   return (
     <>
       <LandingSection />
+      <DisplayHistory />
       <InformativeSection />
     </>
   );
