2165979f9d78d946f5f21c5c93d000cd4414a8e7
object-ions <moshikoatia@gmail.com>
2023-12-09 14:52:06 -0800

    Update components

---
+++ src/components/Display3D.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Display3D = () => {
+  return (
+    <div>
+      <h3> Display3D </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis provident
+        voluptatum quas dolorum modi dolor alias vel, optio quam velit ullam
+        esse, aliquam officia at quibusdam dignissimos quidem pariatur ex fugit
+        sunt hic. Voluptatibus assumenda est praesentium, eligendi deleniti
+        perspiciatis suscipit nihil porro, dolor quas, vero ex voluptas
+        reprehenderit omnis laboriosam. Quaerat optio suscipit, nisi perferendis
+        dolor perspiciatis! Hic perspiciatis exercitationem eos perferendis
+        ipsa? Cupiditate, perferendis dolores, voluptatum maxime rem natus
+        praesentium exercitationem expedita et fugiat, ab ipsam labore veritatis
+        odit quaerat accusamus aspernatur non placeat vitae. Ut, ullam corrupti!
+        Id nemo architecto, nam beatae incidunt sed rerum error optio accusamus
+        dicta eos consequuntur odio quam quaerat nulla nostrum mollitia modi in.
+        Porro exercitationem eligendi ipsa assumenda, magnam ratione asperiores!
+        Explicabo aspernatur dolorem animi sapiente neque, provident esse quos
+        fugiat odit fugit, exercitationem corporis inventore debitis, numquam
+        impedit incidunt sequi nulla officia! Fugit dolore ut nostrum, iste,
+        explicabo, iusto repellat officia quisquam impedit magni aperiam nobis
+        ipsam sapiente error labore distinctio pariatur nihil. Dolor ut aliquam
+        provident libero animi nisi soluta, minima adipisci labore dolorem
+        exercitationem accusantium quibusdam ipsa asperiores nostrum suscipit
+        voluptates ullam! Laborum sint corporis aliquam sunt placeat quasi
+        perferendis nihil ab, in vitae repellendus dolores fuga iure dicta
+        deserunt minima at. Facilis, ipsa. Quasi, cupiditate? Aut et nihil saepe
+        praesentium rem suscipit laborum libero assumenda id quae temporibus,
+        voluptatibus corrupti ea non nesciunt atque incidunt dignissimos rerum?
+        Repellendus voluptas sapiente recusandae soluta culpa cumque est veniam,
+        assumenda cum, adipisci saepe odio quas nulla distinctio! Quod assumenda
+      </p>
+    </div>
+  );
+};
+
+export default Display3D;
--- src/components/pages/DisplayControl.js
+++ src/components/pages/DisplayControl.js
@@ -2,11 +2,13 @@ import React from "react";
 import InformativeSection from "../InformativeSection";
 import LandingSection from "../LandingSection";
 import DisplayHistory from "../DisplayHistory";
+import Display3D from "../Display3D";
 
 const DisplayControl = () => {
   return (
     <>
       <LandingSection />
+      <Display3D />
       <DisplayHistory />
       <InformativeSection />
     </>
