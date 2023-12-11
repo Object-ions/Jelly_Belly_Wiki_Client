7c1e233c0f00da7132fc483d734b0acb1ba59883
object-ions <moshikoatia@gmail.com>
2023-12-10 22:08:12 -0800

    Add 3D scene. WIP.

--- src/components/InformativeSection.js
+++ src/components/InformativeSection.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SplineViewer from "./SplineViewer";
 
 const InformativeSection = () => {
-  return <div className="InformativeSection">InformativeSection</div>;
+  return (
+    <div className="InformativeSection">
+      <SplineViewer url="https://prod.spline.design/mjbobhqsR1jJBonF/scene.splinecode" />
+    </div>
+  );
 };
 
 export default InformativeSection;
---
+++ src/components/SplineViewer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SplineViewer = ({ url }) => {
+  return <spline-viewer url={url}></spline-viewer>;
+};
+
+export default SplineViewer;
