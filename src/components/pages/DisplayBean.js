26514310d6605fdf3794f9753d49dbaabad55430
object-ions <moshikoatia@gmail.com>
2023-12-12 21:08:24 -0800

    Prep file to get populate data to the DOM using fetch

--- src/components/pages/DisplayBean.js
+++ src/components/pages/DisplayBean.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DisplayBean = () => {
+  useEffect(() => {
+    fetchBeans();
+  }, []);
+
+  const fetchBeans = () => {
+    fetch("https://localhost:5001/api/Beans")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return <div>DisplayBean</div>;
 };
 
