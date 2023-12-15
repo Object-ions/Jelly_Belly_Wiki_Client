7076ff908933e7019617c4e564a93f92436ce0e9
object-ions <moshikoatia@gmail.com>
2023-12-15 10:33:39 -0800

    Add fetch data from API

--- src/components/pages/DisplayBean.js
+++ src/components/pages/DisplayBean.js
@@ -1,19 +1,67 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
+import beansReducer from "../../reducers/beans-reducer";
+import { getBeansFailure, getBeansSuccess } from "../../actions/index";
+
+const initialState = {
+  isLoaded: false,
+  beans: [],
+  error: null,
+};
 
 const DisplayBean = () => {
+  const [state, dispatch] = useReducer(beansReducer, initialState);
+
   useEffect(() => {
-    fetchBeans();
+    fetch(`https://localhost:5001/api/Beans?pageIndex=1&pageSize=150`)
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(`${res.status}: ${res.statusText}`);
+        } else {
+          const waitResponse = await res.json();
+          console.log(waitResponse);
+          // return res.json();
+          return waitResponse;
+        }
+      })
+      .then((jsonifiedResponse) => {
+        console.log(jsonifiedResponse);
+        const action = getBeansSuccess(jsonifiedResponse.items);
+        dispatch(action);
+      })
+      .catch((error) => {
+        const action = getBeansFailure(error.message);
+        dispatch(action);
+      });
   }, []);
 
-  const fetchBeans = () => {
-    fetch("https://localhost:5001/api/Beans")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-  };
+  const { error, isLoaded, beans } = state;
 
-  return <div>DisplayBean</div>;
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  } else if (!isLoaded) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className="DisplayBean">
+        <div className="container">
+          <h3>a Little Bit of Bean ...</h3>
+          <div className="bean-grid">
+            {/*  Dynamic */}
+            {beans.map((item, index) => (
+              <div className="item-wrapper" key={index}>
+                <div>
+                  <h4>{item.flavorName}</h4>
+                  <p>{item.description}</p>
+                </div>
+                <img src={item.imageUrl} />
+              </div>
+            ))}
+            {/* END Dynamic */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default DisplayBean;
