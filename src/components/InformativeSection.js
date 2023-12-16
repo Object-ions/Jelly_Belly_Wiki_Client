7773449579a87d022860ae12637d7663596e417a
object-ions <moshikoatia@gmail.com>
2023-12-15 17:57:27 -0800

    Fetch array of beans by specific BeanId

--- src/components/InformativeSection.js
+++ src/components/InformativeSection.js
@@ -12,23 +12,27 @@ const InformativeSection = () => {
   const [state, dispatch] = useReducer(beansReducer, initialState);
 
   useEffect(() => {
-    fetch(`https://localhost:5001/api/Beans/16`)
-      .then(async (res) => {
-        if (!res.ok) {
-          throw new Error(`${res.status}: ${res.statusText}`);
-        } else {
-          const waitResponse = await res.json();
-          console.log(waitResponse);
-          return waitResponse;
-        }
-      })
-      .then((jsonifiedResponse) => {
-        const action = getBeansSuccess([jsonifiedResponse]);
-        dispatch(action);
+    const beanIds = [92, 21, 43, 40, 65, 80];
+
+    Promise.all(
+      beanIds.map((id) =>
+        fetch(`https://localhost:5001/api/Beans/${id}`)
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error(`${res.status}: ${res.statusText}`);
+            }
+            return res.json();
+          })
+          .catch((error) => {
+            dispatch(getBeansFailure(error.message));
+          })
+      )
+    )
+      .then((beans) => {
+        dispatch(getBeansSuccess(beans.filter((bean) => bean)));
       })
       .catch((error) => {
-        const action = getBeansFailure(error.message);
-        dispatch(action);
+        dispatch(getBeansFailure(error.message));
       });
   }, []);
 
@@ -39,17 +43,22 @@ const InformativeSection = () => {
   } else if (!isLoaded) {
     return <h1>Loading...</h1>;
   } else {
-    const bean = beans[0];
     return (
-      <div className="InformativeSection">
-        <div className="item-wrapper">
-          <h3>{bean.flavorName}</h3>
-          <img src={bean.imageUrl} alt={bean.flavorName}></img>
-        </div>
-
-        <div className="item-wrapper">
-          <h3>Explore 100+ beans</h3>
-          {/* arrow */}
+      <div className="InformativeSection container">
+        <h2>Beans</h2>
+        <div className="info-grid ">
+          {beans.map((bean, index) => (
+            <div key={index} className="item-wrapper">
+              <h3>{bean.flavorName}</h3>
+              <div className="img-wrapper">
+                <img src={bean.imageUrl} alt={bean.flavorName} />
+              </div>
+            </div>
+          ))}
+          <div className="item-wrapper">
+            <h3>Explore 100+ beans</h3>
+            {/* arrow */}
+          </div>
         </div>
       </div>
     );
