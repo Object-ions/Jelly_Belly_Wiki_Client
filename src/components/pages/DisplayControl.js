194cc62c2073ae48c8052430955db3352b60cf6f
object-ions <moshikoatia@gmail.com>
2023-12-15 19:10:37 -0800

    Change flow structure

--- src/components/pages/DisplayControl.js
+++ src/components/pages/DisplayControl.js
@@ -3,16 +3,19 @@ import InformativeSection from "../InformativeSection";
 import LandingSection from "../LandingSection";
 import DisplayHistory from "../DisplayHistory";
 import About from "../About";
+import DisplayFact from "./DisplayFact";
 import SplineViewer from "../SplineViewer";
 
 const DisplayControl = () => {
   return (
     <>
       <LandingSection />
+      <About />
       <DisplayHistory />
       <InformativeSection />
+      <DisplayFact />
+
       {/* <SplineViewer url="https://prod.spline.design/mjbobhqsR1jJBonF/scene.splinecode" /> */}
-      <About />
     </>
   );
 };
--- src/components/pages/DisplayFact.js
+++ src/components/pages/DisplayFact.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useReducer } from "react";
+import beansReducer from "../../reducers/beans-reducer";
+import { getBeansFailure, getBeansSuccess } from "../../actions/index";
+
+const initialState = {
+  isLoaded: false,
+  facts: [],
+  error: null,
+};
 
 const DisplayFact = () => {
-  return (
-    <div>DisplayFact</div>
-  )
-}
+  const [state, dispatch] = useReducer(beansReducer, initialState);
 
-export default DisplayFact
+  useEffect(() => {
+    const factIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    Promise.all(
+      factIds.map((id) =>
+        fetch(`https://localhost:5001/api/Facts/${id}`)
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error(`${res.status}: ${res.statusText}`);
+            }
+            return res.json();
+          })
+          .catch((error) => {
+            dispatch(getBeansFailure(error.message));
+          })
+      )
+    )
+      .then((facts) => {
+        dispatch(getBeansSuccess(facts.filter((fact) => fact)));
+      })
+      .catch((error) => {
+        dispatch(getBeansFailure(error.message));
+      });
+  }, []);
+
+  const { error, isLoaded, facts } = state;
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  } else if (!isLoaded) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div>
+        <div>
+          <div>
+            <h2>Explore Facts</h2>
+          </div>
+          {facts.map((fact, index) => (
+            <div key={index} className="item-wrapper">
+              <h3>{fact.title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default DisplayFact;
