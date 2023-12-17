96df203a85bdc9a401e65d8f3d9ddc7e173b9310
object-ions <moshikoatia@gmail.com>
2023-12-16 21:39:56 -0800

    Update component, asset and styling for component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="Contact">
+      <div class="container">
+        <h3>Contact Us</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
