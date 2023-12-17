a5a678c2e525e9779a077e1163e20df2f41872f3
object-ions <moshikoatia@gmail.com>
2023-12-16 19:36:03 -0800

    Update component structure and it's styling

--- src/components/BeanCard.js
+++ src/components/BeanCard.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useReducer } from "react";
 import beansReducer from "./../reducers/beans-reducer";
 import { getSingleBeanFailure, getSingleBeanSuccess } from "./../actions/index";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ErrorComponent from "./ErrorComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 
 const initialState = {
   isLoaded: false,
@@ -47,55 +49,66 @@ const BeanCard = () => {
     return (
       <div className="container">
         <div className="Bean-card">
-          <div className="bean-card-item">
+          <div className="bean-card-wrapper centered">
+            {/* <h3>Flavor Name:</h3> */}
+            <h3>{singleBean.flavorName}</h3>
+          </div>
+          <div className="bean-card-wrapper centered">
+            {/* <p>Description:</p> */}
+            <p>{singleBean.description}</p>
+          </div>
+          <div className="img-wrapper">
+            <img src={singleBean.imageUrl} />
+          </div>
+          <div className="bean-card-wrapper">
             <p>Group Name:</p>
             <p>{singleBean.groupName.join(",")}</p>
           </div>
-          <div className="bean-card-item">
-            <h3>Flavor Name:</h3>
-            <h3>{singleBean.flavorName}</h3>
-          </div>
-          <div className="bean-card-item">
-            <p>Description:</p>
-            <p>{singleBean.description}</p>
-            <div className="img-wrapper">
-              <img src={singleBean.imageUrl} />
-            </div>
-          </div>
-          <div className="bean-card-item">
+          <div className="bean-card-wrapper">
             <p>Ingredients:</p>
             <p>{singleBean.ingredients.join(",")}</p>
           </div>
-          <div className="bean-card-item">
-            <p>Color Group:</p>
-            <p>"{singleBean.colorGroup}"</p>
+          <div className="grid">
+            <div className="bean-card-wrapper">
+              <p>Color Group:</p>
+              <p>"{singleBean.colorGroup}"</p>
+            </div>
+            <div className="bean-card-wrapper">
+              <p>Hexadecimal Color:</p>
+              <div
+                id="bean-bg-color"
+                style={{ backgroundColor: singleBean.backgroundColor }}
+              >
+                {singleBean.backgroundColor}
+              </div>
+            </div>
+            <div className="bean-card-wrapper">
+              <p>Bean ID:</p>
+              <p>{singleBean.beanId}</p>
+            </div>
+            <div className="bean-card-wrapper">
+              <p>Kosher: </p>
+              <p>{singleBean.kosher ? "yes" : "no"}</p>
+            </div>
+            <div className="bean-card-wrapper">
+              {" "}
+              <p>Seasonal:</p>
+              <p>{singleBean.seasonal ? "yes" : "no"}</p>
+            </div>
+            <div className="bean-card-wrapper">
+              <p>Gluten Free: </p>
+              <p>{singleBean.glutenFree ? "yes" : "no"}</p>
+            </div>
+            <div className="bean-card-wrapper">
+              {" "}
+              <p>Sugar Free: </p>
+              <p>{singleBean.sugarFree ? "yes" : "no"}</p>
+            </div>
           </div>
-          <div className="bean-card-item">
-            <p>Hexadecimal Color:</p>
-            <div id="bean-bg-color">{singleBean.backgroundColor}</div>
-          </div>
-          <div className="bean-card-item">
-            <p>Bean ID:</p>
-            <p>{singleBean.beanId}</p>
-          </div>
-          <div className="bean-card-item">
-            <p>Kosher: </p>
-            <p>{singleBean.kosher ? "yes" : "no"}</p>
-          </div>
-          <div className="bean-card-item">
-            {" "}
-            <p>Seasonal:</p>
-            <p>{singleBean.seasonal ? "yes" : "no"}</p>
-          </div>
-          <div className="bean-card-item">
-            <p>Gluten Free: </p>
-            <p>{singleBean.glutenFree ? "yes" : "no"}</p>
-          </div>
-          <div className="bean-card-item">
-            {" "}
-            <p>Sugar Free: </p>
-            <p>{singleBean.sugarFree ? "yes" : "no"}</p>
-          </div>
+          <Link to="/beans">
+            Back to Beans
+            <FontAwesomeIcon icon={faArrowRightLong} />
+          </Link>
         </div>
       </div>
     );
