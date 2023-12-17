05b129b3bc1880efd3aa9d63c13c9875bcfc5253
object-ions <moshikoatia@gmail.com>
2023-12-17 10:46:38 -0800

    Update component and its styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,15 @@ const Contact = () => {
       <div class="Contact-container">
         <div className="Contact-inside">
           <h3>Contact Us</h3>
-          <p>etdkjhfsdkjbfskdnfdfsksjldfjsndvjsndvjnskjdvn</p>
+          <form className="Contact-form">
+            <label for="name">Your Name:</label>
+            <input type="text" name="name" required />
+            <label for="email">Email Address:</label>
+            <input type="email" name="email" required />
+            <label for="text-area">What's on Your Mind?</label>
+            <textarea name="text-area" required />
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </div>
     </div>
