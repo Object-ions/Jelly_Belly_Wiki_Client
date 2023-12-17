6305382a5401f00ccf45083a5af3ebfb7796c3ff
object-ions <moshikoatia@gmail.com>
2023-12-16 21:14:22 -0800

    Update structure

--- src/components/ErrorComponent.js
+++ src/components/ErrorComponent.js
@@ -4,7 +4,7 @@ import errorImg from "../img/single-bean-filled-sad.png";
 const ErrorComponent = ({ error }) => {
   return (
     <div className="error">
-      <h1>Oh no! there's an error:</h1>
+      <h1>Oh no, an error:</h1>
       <h1>{error}</h1>
       <img src={errorImg} />
     </div>
