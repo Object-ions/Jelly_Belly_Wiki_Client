1e320873a5b8f8ec01ce061a9d190c4e116e57ff
object-ions <moshikoatia@gmail.com>
2023-12-17 10:11:36 -0800

    Edit component and its styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,11 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="Contact">
-      <div class="container">
-        <h3>Contact Us</h3>
+      <div class="Contact-container">
+        <div className="Contact-inside">
+          <h3>Contact Us</h3>
+          <p>etdkjhfsdkjbfskdnfdfsksjldfjsndvjsndvjnskjdvn</p>
+        </div>
       </div>
     </div>
   );
