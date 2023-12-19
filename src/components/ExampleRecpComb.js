9bf0accfd88b025808f380d7906d305814816558
object-ions <moshikoatia@gmail.com>
2023-12-19 12:34:11 -0800

    Add new component that contain 2 nested components

---
+++ src/components/ExampleRecpComb.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ExampleRecipe from "./Recipe";
+import ExampleCombination from "./Combination";
+
+const ExampleRecpComb = () => {
+  return (
+    <div className="container">
+      <div className="ExampleRecpComb">
+        <div className="ExampleRecipe">
+          <h2>Explore More</h2>
+          <ExampleRecipe />
+          <ExampleCombination />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExampleRecpComb;
