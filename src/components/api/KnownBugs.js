7efb11996f1b47cd30507e5a2f00d6b936eed2a5
object-ions <moshikoatia@gmail.com>
2023-12-19 18:38:39 -0800

    Update components to plan API documentation with routes

--- src/components/api/KnownBugs.js
+++ src/components/api/KnownBugs.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const knownBugs = () => {
-  return <div>knownBugs</div>;
+  return (
+    <div>
+      <h3>Known Bugs</h3>
+      <p>no known bugs</p>
+    </div>
+  );
 };
 
 export default knownBugs;
--- src/components/pages/DisplayApi.js
+++ src/components/pages/DisplayApi.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./DisplayApi.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 import Setup from "../api/Setup";
 import Opening from "../api/Opening";
 import DataSample from "../api/DataSample";
@@ -17,6 +19,53 @@ import Examples from "../api/Examples";
 const DisplayApi = () => {
   return (
     <>
+      <div className="container">
+        <nav className="sidebar">
+          <ul>
+            <li>
+              <Link to="/setup">Setup</Link>
+            </li>
+            <li>
+              <Link to="/opening">Opening</Link>
+            </li>
+            <li>
+              <Link to="/datasample">Data Sample</Link>
+            </li>
+            <li>
+              <Link to="/endpoints">Endpoints</Link>
+            </li>
+            <li>
+              <Link to="/knownbugs">Known Bugs</Link>
+            </li>
+            <li>
+              <Link to="/license">License</Link>
+            </li>
+            <li>
+              <Link to="/pagination">Pagination</Link>
+            </li>
+            <li>
+              <Link to="/query">Query</Link>
+            </li>
+            <li>
+              <Link to="/techused">Technologies Used</Link>
+            </li>
+            <li>
+              <Link to="/description">Description</Link>
+            </li>
+            <li>
+              <Link to="/features">Features</Link>
+            </li>
+            <li>
+              <Link to="/createdatabase">Create Database</Link>
+            </li>
+            <li>
+              <Link to="/examples">Examples</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* break */}
       <Opening />
       <Description />
       <Features />
