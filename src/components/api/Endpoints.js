8d7ce2c5ee2494dc6bf757104d9709c13622985b
object-ions <moshikoatia@gmail.com>
2023-12-20 12:03:56 -0800

    Add conditional rendering

--- src/components/api/Endpoints.js
+++ src/components/api/Endpoints.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import EndpointsBeans from "./EndpointsBeans";
 import EndpointsRecipes from "./EndpointsRecipes";
 import EndpointsCombinations from "./EndpointsCombinations";
 import EndpointsFacts from "./EndpointsFacts";
 import EndpointsMilestones from "./EndpointsMilestones";
 
-const endpoints = () => {
+const Endpoints = () => {
+  const [selectedEndpoint, setSelectedEndpoint] = useState("Beans");
+
+  const handleSelectEndpoint = (endpoint) => {
+    setSelectedEndpoint(endpoint);
+  };
+
   return (
     <>
       <h2>Endpoints</h2>
       <ol>
         <h3>List of Endpoints:</h3>
-        <li>"Beans"</li>
-        <li>"Facts"</li>
-        <li>"Recipes"</li>
-        <li>"Combinations"</li>
-        <li>"History"</li>
+        <li onClick={() => handleSelectEndpoint("Beans")}>Beans</li>
+        <li onClick={() => handleSelectEndpoint("Facts")}>Facts</li>
+        <li onClick={() => handleSelectEndpoint("Recipes")}>Recipes</li>
+        <li onClick={() => handleSelectEndpoint("Combinations")}>
+          Combinations
+        </li>
+        <li onClick={() => handleSelectEndpoint("Milestones")}>History</li>
       </ol>
       <h3>HTTP Request Structure</h3>
-      <EndpointsBeans />
-      {/* <EndpointsFacts /> */}
-      {/* <EndpointsRecipes /> */}
-      {/* <EndpointsCombinations /> */}
-      {/* <EndpointsMilestones /> */}
+      {selectedEndpoint === "Beans" && <EndpointsBeans />}
+      {selectedEndpoint === "Facts" && <EndpointsFacts />}
+      {selectedEndpoint === "Recipes" && <EndpointsRecipes />}
+      {selectedEndpoint === "Combinations" && <EndpointsCombinations />}
+      {selectedEndpoint === "Milestones" && <EndpointsMilestones />}
     </>
   );
 };
 
-export default endpoints;
+export default Endpoints;
