b8d19ad8f03525d1bfb08b4a78eedcacaf812641
object-ions <moshikoatia@gmail.com>
2023-12-26 18:44:37 -0800

    Update tags to follow React conventions

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,15 +4,15 @@ import mandarin from "../img/mandarin.png";
 const Contact = () => {
   return (
     <div className="Contact">
-      <div class="Contact-container">
+      <div className="Contact-container">
         <div className="Contact-inside">
           <h3>Contact Us</h3>
           <form className="Contact-form">
-            <label for="name">Your Name:</label>
+            <label htmlFor="name">Your Name:</label>
             <input type="text" name="name" required />
-            <label for="email">Email Address:</label>
+            <label htmlFor="email">Email Address:</label>
             <input type="email" name="email" required />
-            <label for="text-area">What's on Your Mind?</label>
+            <label htmlFor="text-area">What's on Your Mind?</label>
             <textarea name="text-area" required />
             <button type="submit">Submit</button>
           </form>
